7b1915bf718d92d0eb92138bf26ca369dcbd4f74
LAPTOP-8JFNIJ8D\rafae <rafael.nercessian@thortongreen.com>
2022-05-26 19:02:57 -0300

    Adding Context for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from '../src/styles/global'
 import theme from '../src/styles/theme'
+import { RouterContext } from 'next/dist/shared/lib/router-context' // next 12
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider
+  }
+}
 
 export const decorators = [
   (Story) => (
