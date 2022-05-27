6047a80b4989eebbb992fafd59ecb647c2712860
LAPTOP-8JFNIJ8D\rafae <rafael.nercessian@thortongreen.com>
2022-05-27 16:52:08 -0300

    Testing button

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react'
 import Button from '.'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 export default {
   title: 'Button',
@@ -7,6 +8,9 @@ export default {
   argTypes: {
     children: {
       type: 'string'
+    },
+    icon: {
+      type: 'symbol'
     }
   }
 } as Meta
@@ -15,4 +19,12 @@ export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy Now'
+}
+
+export const WithIcon: Story = (args) => <Button {...args} />
+
+WithIcon.args = {
+  size: 'small',
+  children: 'Buy Now',
+  icon: <AddShoppingCart />
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,8 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
-type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'>
+type WrapperProps = {
+  hasIcon: boolean
+} & Pick<ButtonProps, 'size' | 'fullWidth'>
 
 const wrapperModifiers = {
   small: (theme: DefaultTheme) => css`
@@ -19,18 +21,33 @@ const wrapperModifiers = {
   `,
   fullWidth: () => css`
     width: 100%;
+  `,
+  withIcon: (theme: DefaultTheme) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      width: 1.5rem;
+
+      & + span {
+        margin-left: ${theme.spacings.xxsmall};
+      }
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth }) => css`
+  ${({ theme, size, fullWidth, hasIcon }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+    cursor-pointer: pointer;
 
-    ${!!size && wrapperModifiers[size](theme)}}
-    ${!!fullWidth && wrapperModifiers.fullWidth}}
+    ${!!size && wrapperModifiers[size](theme)}
+    ${!!fullWidth && wrapperModifiers.fullWidth}
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)}
   `}
 `
