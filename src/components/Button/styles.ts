9afd10a566d453a22c13910960aae0d05ef1672b
LAPTOP-8JFNIJ8D\rafae <rafael.nercessian@thortongreen.com>
2022-05-28 00:35:51 -0300

    Button acting as link or button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
+
 type WrapperProps = {
   hasIcon: boolean
 } & Pick<ButtonProps, 'size' | 'fullWidth'>
@@ -42,12 +43,16 @@ export const Wrapper = styled.button<WrapperProps>`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
+    cursor: pointer;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
-    cursor-pointer: pointer;
 
-    ${!!size && wrapperModifiers[size](theme)}
-    ${!!fullWidth && wrapperModifiers.fullWidth}
-    ${!!hasIcon && wrapperModifiers.withIcon(theme)}
+    &:hover {
+      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+    }
+
+    ${!!size && wrapperModifiers[size](theme)};
+    ${!!fullWidth && wrapperModifiers.fullWidth()};
+    ${!!hasIcon && wrapperModifiers.withIcon(theme)};
   `}
 `
