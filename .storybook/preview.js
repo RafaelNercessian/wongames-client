3f2b7b80eb6aa8d6482776c421004a3d36570fb6
LAPTOP-8JFNIJ8D\rafae <rafael.nercessian@thortongreen.com>
2022-05-30 20:40:25 -0300

    Adding background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,13 +6,26 @@ import { RouterContext } from 'next/dist/shared/lib/router-context' // next 12
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
+  },
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
   }
 }
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
+      <GlobalStyles removeBg />
       <Story />
     </ThemeProvider>
   )
