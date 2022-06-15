b82b92aeac9ee386766c72d03d7f9e4752f4864f
LAPTOP-8JFNIJ8D\rafae <rafael.nercessian@thortongreen.com>
2022-06-15 00:53:16 -0300

    Creating page for games

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,3 +1,24 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: space-between;
+`
+
+export const SectionFooter = styled.section`
+  ${({ theme }) => css`
+    margin-top: ${theme.spacings.large};
+    padding-bottom: ${theme.spacings.xsmall};
+    padding-top: ${theme.spacings.xxlarge};
+    background-color: ${theme.colors.white};
+    clip-path: polygon(0 5%, 100% 0%, 100% 100%, 0 100%);
+
+    ${media.greaterThan('medium')`
+      padding-top: calc(${theme.spacings.xxlarge} * 2);
+      clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
+    `}
+  `}
+`
